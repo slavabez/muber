628882b2654d65dfbed418dfee978fdb0e92d1dc
Slava Bezgachev <bezgachev@gmail.com>
2018-01-05 09:27:22 +0600

    Simple test complete

---
+++ test/app_test.js
@@ -0,0 +1,14 @@
+const assert = require('assert');
+const request = require('supertest');
+const app = require('../app');
+
+describe('The express app', () => {
+    it('handles a GET request to the /api route', (done) => {
+        request(app)
+            .get('/api')
+            .end((err, response) => {
+                assert(response.body.hi === "there");
+                done();
+            });
+    })
+});
